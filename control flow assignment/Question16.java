9b7d6aabd10430351425e8828a2902adeac721ce
DeepakDeveloper2026 <dy5055290@gmail.com>
2025-06-12 12:11:47 +0530

    day4

---
+++ control flow assignment/Question16.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Question16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        int counter = 1;
+        while (counter <= number) {
+            if (number % counter == 0) {
+                System.out.println(counter);
+            }
+            counter++;
+        }
+    }
+}
